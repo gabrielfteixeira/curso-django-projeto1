1b8de37789872eb3f428b4bab9d2c3d365d0e7ed
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2023-06-22 02:05:42 -0300

    new things

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request,'recipes/home.html',context={
         'name': 'Gabriel Teixeira'
     })
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
